3426309c84268276e9ea92ac45d1d14e6ef80635
Shane Schisler <shane.schisler@contrastsecurity.com>
2021-11-15 20:33:23 -0500

    adding a vuln

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -25,4 +25,26 @@ public class Cowsay {
     }
     return output.toString();
   }
+
+  public static String run2(String input) {
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    String cmd = "/usr/games/cowsay '" + input + "'";
+    System.out.println(cmd);
+    processBuilder.command("bash", "-c", cmd);
+
+    StringBuilder output = new StringBuilder();
+
+    try {
+      Process process = processBuilder.start();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+      String line;
+      while ((line = reader.readLine()) != null) {
+        output.append(line + "\n");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return output.toString();
+  }
 }
